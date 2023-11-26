baec0e3229c4ed1a2097ef1dc1caab06433f3095
Ever Frank <ever.avendano@unmsm.edu.pe>
2023-11-26 13:10:56 -0500

    version cors

--- scripts/js-route-search-san-marcos/api.js
+++ scripts/js-route-search-san-marcos/api.js
@@ -1,8 +1,10 @@
+const backendUrl = "https://nesoapps.onrender.com";
+
 export async function sendDataToServer(nodesArray, idNodeStart, idNodeEnd) {
     try {
         let data = { 'node_start_id': idNodeStart, 'node_end_id': idNodeEnd, 'nodes': nodesArray };
 
-        const response = await fetch("/route-min/api/calculate-routes-min", {
+        const response = await fetch(`${backendUrl}/route-min/api/calculate-routes-min`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
--- scripts/js-schedule-generator/api.js
+++ scripts/js-schedule-generator/api.js
@@ -1,8 +1,10 @@
+const backendUrl = "https://nesoapps.onrender.com";
+
 import createSchedules from "./schedule_actions.js";
 
 export async function sendCoursesToTheServer(courses) {
     try {
-        const response = await fetch("/schedule-generator/api/data", {
+        const response = await fetch(`${backendUrl}/schedule-generator/api/data`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -30,7 +32,7 @@ export async function sendArchiveToTheServer(file) {
     formData.append("pdf", file, file.name);
 
     try {
-        const response = await fetch("/schedule-generator/api/file-upload", {
+        const response = await fetch(`${backendUrl}/schedule-generator/api/file-upload`, {
             method: "POST",
             body: formData
         });
