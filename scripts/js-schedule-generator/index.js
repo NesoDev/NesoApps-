8c711853f2152e1aa49515ff826bc8182ab312f7
Ever Frank <ever.avendano@unmsm.edu.pe>
2023-12-11 01:17:12 -0500

    Update index.js

--- scripts/js-schedule-generator/index.js
+++ scripts/js-schedule-generator/index.js
@@ -5,7 +5,277 @@ import { sendArchiveToTheServer } from "./api.js";
 
 let body = document.querySelector("body");
 let tableBody = document.querySelector("tbody");
-let courses = [];
+let courses = [
+    {
+        "nombre": "curso 1",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Lunes",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "practica": {
+                    "dia": "Lunes",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "preferencia": 2
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Martes",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Miércoles",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "preferencia": 1
+            }
+        ]
+    },
+    {
+        "nombre": "curso 2",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Jueves",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "practica": {
+                    "dia": "Viernes",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "preferencia": 1
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Viernes",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Viernes",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 2
+            }
+        ]
+    },
+
+    {
+        "nombre": "curso 3",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Miércoles",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Miércoles",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 2
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Sábado",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "practica": {
+                    "dia": "Sábado",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "preferencia": 1
+            }
+        ]
+    },
+    {
+        "nombre": "curso 4",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Sábado",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Sábado",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 1
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Lunes",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Lunes",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 2
+            }
+        ]
+    },
+    {
+        "nombre": "curso 5",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Miércoles",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Miércoles",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 2
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Jueves",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "practica": {
+                    "dia": "Jueves",
+                    "inicio": "16:00",
+                    "fin": "18:00"
+                },
+                "preferencia": 1
+            }
+        ]
+    },
+    {
+        "nombre": "curso 6",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Martes",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "practica": {
+                    "dia": "Martes",
+                    "inicio": "12:00",
+                    "fin": "14:00"
+                },
+                "preferencia": 3
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Miércoles",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "practica": {
+                    "dia": "Miércoles",
+                    "inicio": "12:00",
+                    "fin": "14:00"
+                },
+                "preferencia": 2
+            }
+        ]
+    },
+    {
+        "nombre": "curso 7",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Jueves",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "practica": {
+                    "dia": "Jueves",
+                    "inicio": "12:00",
+                    "fin": "14:00"
+                },
+                "preferencia": 1
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Viernes",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "practica": {
+                    "dia": "Viernes",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "preferencia": 2
+            }
+        ]
+    },
+    {
+        "nombre": "curso 8",
+        "secciones": [
+            {
+                "id": 0,
+                "teoria": {
+                    "dia": "Lunes",
+                    "inicio": "12:00",
+                    "fin": "14:00"
+                },
+                "practica": {
+                    "dia": "Lunes",
+                    "inicio": "14:00",
+                    "fin": "16:00"
+                },
+                "preferencia": 3
+            },
+            {
+                "id": 1,
+                "teoria": {
+                    "dia": "Martes",
+                    "inicio": "08:00",
+                    "fin": "10:00"
+                },
+                "practica": {
+                    "dia": "Martes",
+                    "inicio": "10:00",
+                    "fin": "12:00"
+                },
+                "preferencia": 1
+            }
+        ]
+    }
+];
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
